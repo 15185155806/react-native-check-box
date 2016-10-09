fb0df40e55014df3f28938bf5d0396053e50f28d
cc <183952887@qq.com>
2016-10-10 01:05:29 +0800

    add rightText and IconFont of support

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ import {
     Text,
     TouchableHighlight
 } from 'react-native'
+// 导入图标库
+import IconFA from 'react-native-vector-icons/FontAwesome'; // 参考地址: http://fontawesome.io/icons/
 
 
 export default class CheckBox extends Component {
@@ -25,21 +27,43 @@ export default class CheckBox extends Component {
         }
     }
 
+    /**
+     * 定义类属性
+     * 作用: 约束调用该组件的时候设置的属性值的类型
+     * propTypes是默认写法
+     */
     static propTypes = {
         ...View.propTypes,
         leftText: React.PropTypes.string,
         leftTextView: React.PropTypes.element,
+        rightText: React.PropTypes.string,
+        rightTextStyle: React.PropTypes.style,
         checkedImage: React.PropTypes.element,
         unCheckedImage: React.PropTypes.element,
         onClick: React.PropTypes.func.isRequired,
-        isChecked: React.PropTypes.bool
-
+        isChecked: React.PropTypes.bool,
+        isIcon: React.PropTypes.bool,
+        iconStyle: React.PropTypes.style,
+        checkedIconName: React.PropTypes.string,
+        unCheckedIconName: React.PropTypes.string,
     }
+
+    /**
+     * 用于设置属性的默认值
+     * @type {Object}
+     */
     static defaultProps = {
         isChecked: false,
+        isIcon: false,
+        checkedIconName: 'check-square-o',
+        unCheckedIconName: 'square-o',
     }
 
+    /**
+     * 渲染左侧控件: 文字或者图标
+     */
     _renderLeft() {
+        if (this.props.rightText) return this.props.isIcon ? this._renderIcon() : return this._renderImage();
         if (this.props.leftTextView)return this.props.leftTextView;
         if (!this.props.leftText)return null;
         return (
@@ -47,6 +71,20 @@ export default class CheckBox extends Component {
         )
     }
 
+    /**
+     * 渲染右侧控件: 文字或者Icon
+     */
+    _renderRight() {
+        if (this.props.leftText) return this._renderImage();
+        if (!this.props.rightText)return null;
+        return (
+            <Text style={styles.rightText}>{this.props.rightText}</Text>
+        )
+    }
+
+    /**
+     * 渲染图片组件
+     */
     _renderImage() {
         if (this.state.isChecked) {
             return this.props.checkedImage ? this.props.checkedImage : this.genCheckedImage();
@@ -55,6 +93,24 @@ export default class CheckBox extends Component {
         }
     }
 
+    /**
+     * 渲染Icon组件
+     */
+    _renderIcon() {
+        if (this.state.isChecked) {
+            return (
+                <IconFA name={this.props.checkedIconName} style={this.iconStyle}/>
+            )
+        } else {
+            return (
+                <IconFA name={this.props.unCheckedIconName} style={this.iconStyle}/>
+            )
+        }
+    }
+
+    /**
+     * 获取图片资源
+     */
     genCheckedImage() {
         var source = this.state.isChecked ? require('./img/ic_check_box.png') : require('./img/ic_check_box_outline_blank.png');
 
@@ -79,7 +135,7 @@ export default class CheckBox extends Component {
             >
                 <View style={styles.container}>
                     {this._renderLeft()}
-                    {this._renderImage()}
+                    {this._renderRight()}
                 </View>
             </TouchableHighlight>
         )
@@ -89,10 +145,11 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'center'
-        // backgroundColor:'gray'
     },
     leftText: {
         flex: 1,
-        // backgroundColor:'red',
+    },
+    rightText: {
+        flex: 1,
     }
 })
